80d7db84e4511dd48e958a9f80510e2fd8299100
Ganesh Bagra <ganeshbagra003@gmail.com>
2025-08-31 02:32:56 +0530

    add some models

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose,{model, Schema} from "mongoose"
+import aggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema({
+    videoFile : {
+        type : String,
+        required : [true, "Video is required"]
+    },
+    thumbnail : {
+        type : String,
+        required : [true, "Thumbnail is required"]
+    },
+    title : {
+        type : String,
+        required : [true, "title is required"]
+    },
+    description : {
+        type : String,
+        required : [true, "description is required"]
+    },
+    duration : {
+        type : Number, // get from where the video is saved
+        required : true
+    },
+    views : {
+        type : Number,
+        default : 0
+    },
+    isPublished : {
+        type : Boolean,
+        default : true
+    },
+    videoOwner : {
+        type : Schema.Types.ObjectId,
+        ref : "User"
+    }
+},{timestamps : true})
+
+videoSchema.plugin(aggregatePaginate)
+
+export const Video = model("Video",videoSchema);
